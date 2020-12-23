1a9550da0ba2e06c8f640151883351ca94029530
sylvain-schmitt <69637905+sylvain-schmitt@users.noreply.github.com>
2020-12-23 12:56:37 +0100

    feat: add delete pin route and form

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -75,4 +75,16 @@ class PinsController extends AbstractController
             'pin' => $pin
         ]);
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods={"DELETE"})
+     */
+    public function delete(Pin $pin, EntityManagerInterface $em)
+    {
+        $em->remove($pin);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+
 }
